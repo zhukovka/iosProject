30cccae7f3757b196459f1932c77e3428a50c612
zhukovka <helen.zhukova@gmail.com>
2017-11-13 17:30:57 +0200

    simple table view example

--- One/ViewController.swift
+++ One/ViewController.swift
@@ -8,7 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    private let dwarves = [
+        "Sleepy", "Sneezy", "Bashful", "Happy",
+        "Doc", "Grumpy", "Dopey",
+        "Thorin", "Dorin", "Nori", "Ori",
+        "Balin", "Dwalin", "Fili", "Kili",
+        "Oin", "Gloin", "Bifur", "Bofur",
+        "Bombur"
+    ]
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
+    // MARK:-
+    // MARK: Table View Data Source Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dwarves.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        if (cell == nil) {
+            cell = UITableViewCell(
+                style: UITableViewCellStyle.default,
+                reuseIdentifier: simpleTableIdentifier)
+        }
+        
+        cell?.textLabel?.text = dwarves[indexPath.row]
+        return cell!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +47,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
